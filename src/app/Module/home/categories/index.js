057d50c3fd5c5cca8991c62af12d6d7b93846786
oluwaseni <oluwasenijbd@gmail.com>
2019-07-09 12:03:13 +0100

    pushing

---
+++ src/app/Module/home/categories/index.js
@@ -0,0 +1,12 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    // document.getElementById("openMenu").style.display = "block";
+    // document.getElementById("closeMenu").style.display = "inline";
+    
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
